ba4e8e2be671ea2b59e12d12ef4e5646b1cc9533
Seda Ünal <sedaylmz06@outlook.com>
2022-11-23 17:21:19 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import MainContent from "./MainContent";
+
+import SideMenu from "./SideMenu";
+import TopMenu from "./TopMenu";
+
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <TopMenu />
+
+      <SideMenu />
+
+      <MainContent />
+
+      {/* What components go here? */}
+    </div>
+  );
 }
 
 export default App;
